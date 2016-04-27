cb552d328b6654eff5edcf93028c1f756cb2198f
fernandoescolar <f.escolar@hotmail.com>
2016-04-27 18:56:34 +0200

    added azure blob storage file manager code

--- Tokiota.Store.Demo.Infrastructure.Azure/FileManager.cs
+++ Tokiota.Store.Demo.Infrastructure.Azure/FileManager.cs
@@ -1,33 +1,68 @@
 ﻿namespace Tokiota.Store.Demo.Infrastructure.Azure
 {
-    using System;
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Blob;
+    using System.Configuration;
     using System.IO;
 
     internal class FileManager : IFileManager
     {
+        private const string AzureStorageConnectionStringKey = "storage:StorageConnectionString";
+        private static readonly string ContainerName = ConfigurationManager.AppSettings["storage:StorageBlobContainerName"];
+        private readonly CloudStorageAccount storageAccount;
+
+        public FileManager()
+        {
+            this.storageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings[AzureStorageConnectionStringKey].ConnectionString);
+        }
+
         public void DeleteFile(string filename)
         {
-            throw new NotImplementedException();
+            var blobName = Path.GetFileName(filename);
+            var container = this.GetOrCreateContainer();
+            var blockBlob = container.GetBlockBlobReference(blobName);
+            blockBlob.Delete();
         }
 
         public bool Exists(string filename)
         {
-            throw new NotImplementedException();
+            var blobName = Path.GetFileName(filename);
+            var container = this.GetOrCreateContainer();
+            var blockBlob = container.GetBlockBlobReference(blobName);
+            return blockBlob.Exists();
         }
 
         public string GetFileUrl(string filename)
         {
-            throw new NotImplementedException();
+            var blobName = Path.GetFileName(filename);
+            var container = this.GetOrCreateContainer();
+            var blockBlob = container.GetBlockBlobReference(blobName);
+            return blockBlob.Uri.ToString();
         }
 
         public Stream OpenFile(string filename)
         {
-            throw new NotImplementedException();
+            var blobName = Path.GetFileName(filename);
+            var container = this.GetOrCreateContainer();
+            var blockBlob = container.GetBlockBlobReference(blobName);
+            return blockBlob.OpenRead();
         }
 
         public void SaveFile(string filename, Stream stream)
         {
-            throw new NotImplementedException();
+            var blobName = Path.GetFileName(filename);
+            var container = this.GetOrCreateContainer();
+            var blockBlob = container.GetBlockBlobReference(blobName);
+            blockBlob.UploadFromStream(stream);
+        }
+
+        private CloudBlobContainer GetOrCreateContainer()
+        {
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference(ContainerName);
+            container.CreateIfNotExists();
+            container.SetPermissions(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+            return container;
         }
     }
 }
