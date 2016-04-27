94cba0a8eb228ec0f27a503e0bfd9684137a7397
fernandoescolar <f.escolar@hotmail.com>
2016-04-27 19:39:52 +0200

    added the reliable azure sql strategy for EntityFramework operations

---
+++ Tokiota.Store.Demo.Infrastructure.Azure/SqlAzureDbConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace Tokiota.Store.Demo.Infrastructure.Azure
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.SqlServer;
+
+    public class SqlAzureDbConfiguration : DbConfiguration
+    {
+        public SqlAzureDbConfiguration()
+        {
+            SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy(3, TimeSpan.FromSeconds(10)));
+        }
+    }
+}
